a83b80e5b99df2a4675b6b17b6e4eb569f3a416a
Theuns Coetzee <ipokkel@gmail.com>
2019-06-20 14:14:04 +0200

    concatenate string instead of printf

--- pmpro-set-expiration-dates.php
+++ pmpro-set-expiration-dates.php
@@ -30,11 +30,7 @@ function pmprosed_pmpro_membership_level_after_other_settings()
         $set_expiration_date = "";
     ?>
     <h3 class="topborder"><?php _e( 'Set Expiration Date', 'pmpro-set-expiration-dates' ); ?></h3>
-    <p><?php printf( 
-        __( 'To have this level expire on a specific date, enter it below in YYYY-MM-DD format. %1$sNote:%2$s You must also set an expiration date above (e.g. 1 Year) which will be overwritten by the value below.', 'pmpro-set-expiration-dates' ),
-        '<strong>',
-        '</strong>'
-        ); ?></p>
+    <p><?php _e( 'To have this level expire on a specific date, enter it below in YYYY-MM-DD format', 'pmpro-set-expiration-dates' ); ?>. <strong><?php _e( 'Note:', 'pmpro-set-expiration-dates' ); ?></strong> <?php _e( 'You must also set an expiration date above (e.g. 1 Year) which will be overwritten by the value below.', 'pmpro-set-expiration-dates' ); ?></p>
     <table>
         <tbody class="form-table">
         <tr>
